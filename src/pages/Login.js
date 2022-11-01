23c0faa100b27487a20e53268a517f687465c82b
Enio Schaefer <enio100schaefer@hotmail.com>
2022-11-01 19:00:13 -0300

    [Deploy]:rocket: Setup de Redux e Requisito 01 Concluidos

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userLogin } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleLogin = () => {
+    const { email } = this.state;
+    const { actionLogin, history } = this.props;
+
+    actionLogin(email);
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const regexEmail = /.+@.+\..+/g;
+    const minLength = 6;
+
+    const checkPassword = password.length >= minLength;
+    const checkEmail = regexEmail.test(email);
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            {' '}
+            <input
+              type="email"
+              data-testid="email-input"
+              name="email"
+              id="email"
+              value={ email }
+              onChange={ (event) => this.handleChange(event) }
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            {' '}
+            <input
+              type="password"
+              data-testid="password-input"
+              name="password"
+              id="password"
+              value={ password }
+              onChange={ (event) => this.handleChange(event) }
+            />
+          </label>
+          <button
+            type="button"
+            name="loginButton"
+            id="loginButton"
+            onClick={ () => this.handleLogin() }
+            disabled={ !(checkPassword && checkEmail) }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  actionLogin: (email) => dispatch(userLogin(email)),
+});
+
+Login.propTypes = {
+  actionLogin: PropTypes.func.isRequired,
+  history: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+export const WALLET_ADDITION = 'WALLET_ADDITION';
+
+export const userLogin = (email) => ({ type: USER_LOGIN, email });
+
+export const walletAddition = (addData) => ({ type: WALLET_ADDITION, addData });
+
+// description, category, value, payMethod, coin
